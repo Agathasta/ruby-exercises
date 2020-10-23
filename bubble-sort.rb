17ce7058af31bb392a471fd6044fe3bb321889b6
agathasta <kalidonia@hotmail.com>
2020-10-23 11:24:42 +0200

    Add Bubble-sort v.1.0

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,5 +1,14 @@
-def bubble_sort
-
+def bubble_sort(arr)
+    until arr == arr.sort do
+        for i in 0...arr.length - 1
+            pair = [arr[i], arr[i + 1]]
+            pair.sort!
+            arr.delete_at(i+1)
+            arr.delete_at(i)
+            arr.insert(i, pair[0], pair[1])
+        end
+    end
+    p arr
 end
 
 bubble_sort([4,3,78,2,0,2])
