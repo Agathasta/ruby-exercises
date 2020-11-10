4c91562aea168d9f20238e91408f0c8a766b7fe1
agathasta <kalidonia@hotmail.com>
2020-11-10 23:49:19 +0100

    Finish complete working version v.2.0

--- 3_tower_of_hanoi.rb
+++ 3_tower_of_hanoi.rb
@@ -1,22 +1,4 @@
-# program ToH
-
-#   √ ask user num of disks   GAME
-#   √ create board    BOARD
-#   √ display boards    BOARD
-#   loop do   GAME
-#     √ ask user coordinates   GAME
-#     if position is not empty    BOARD
-#       display message
-#     elsif there is a smaller disk before    BOARD
-#       display message
-#     else
-#       change board    BOARD
-#     end
-#     display board    BOARD
-#     if last pole has all the disks
-#       display message
-#       stop loop
-#   end
+# frozen_string_literal: true
 
 class Game
   def initialize
@@ -25,21 +7,23 @@ class Game
 
   def play
     until @board.winning?
-      @board.coordinates?
+      @board.coordinates
       @board.new_board
+      @board.display
     end
+    puts 'CONGRATULATIONS!!'
+    puts
   end
-
 end
 
 class Board
   attr_accessor :board
 
   def initialize
-    puts "How many disks?"
+    puts 'How many disks?'
     @n = gets.chomp.to_i
-    @board = Array.new(3){Array.new(@n)}
-    @board[0] = (1..@n).map {|n| "o" * n }
+    @board = Array.new(3) { Array.new(@n) }
+    @board[0] = (1..@n).map { |n| 'o' * n }
     @round = 1
     display
   end
@@ -47,63 +31,75 @@ class Board
   def display
     puts
     puts "1\t2\t3"
-    puts "__________________"
+    puts '__________________'
     i = 0
     while i < @n
       puts "#{@board[0][i]}\t#{@board[1][i]}\t#{@board[2][i]}"
       i += 1
     end
-    puts     
+    puts
   end
 
-  def coordinates?
+  def coordinates
     if @round == 1
-      puts "To move the disks, input two numbers separated by a space:"
-      puts  "the first one is the column you take the disk FROM,"
-      puts  "the second is the column you move the disk TO."
-      puts "V.gr.: 1 3 will move the disk from column 1 to column 3."
+      puts 'To move the disks, input two numbers separated by a space:'
+      puts    'the first one is the column you take the disk FROM,'
+      puts    'the second is the column you move the disk TO.'
+      puts 'V.gr.: 1 3 will move the disk from column 1 to column 3.'
       puts
-      @round = "x"
+      @round = 'x'
     end
     loop do
-      puts "Where to move?"
-      @coord = gets.chomp.split(" ").map{|c| c.to_i - 1}
-      break if check_coordinates_format && check_coordinates_start
+      puts 'Where to move?'
+      coord = gets.chomp.split(' ').map { |c| c.to_i - 1 }
+      @column_nr_from = coord[0]
+      @column_nr_to = coord[1]
+      next unless check_coordinates_format
+
+      @column_from = @board[@column_nr_from]
+      @column_to = @board[@column_nr_to]
+      @disk = @column_from.find { |d| !d.nil? }
+      break if check_coordinates_position
     end
   end
 
   def check_coordinates_format
-    if ((0..2).include? (@coord[0])) && ((0..2).include? (@coord[1]))
-      true
+    if !@column_nr_from.nil? && !@column_nr_to.nil?
+      true if (0..2).include?(@column_nr_from) && (0..2).include?(@column_nr_to)
     else
-      puts "Wrong input!"
+      puts 'Wrong input!'
     end
+  end
+
+  def check_coordinates_position
+    check_coordinates_start && check_coordinates_finish
   end
 
   def check_coordinates_start
-    if @board[@coord[0]][@n - 1].nil?
-      puts "No disk on that column"
-    else 
+    if @column_from[@n - 1].nil?
+      puts "No disk on column #{@column_nr_from + 1}"
+    else
       true
     end
   end
 
+  def check_coordinates_finish
+    if @column_to[@n - 1].nil? || @disk < @column_to.find { |d| !d.nil? }
+      true
+    else
+      puts 'A disk can only be put on top of a bigger one'
+    end
+  end
 
   def new_board
-    column_0 = @board[@coord[0]]
-    column_1 = @board[@coord[1]]
-
-    @disk = column_0.find {|d| !d.nil?}
-    column_0[column_0.index(@disk)] = nil
-
-    column_1.shift
-    idx = column_1.find_index {|d| !d.nil?} || @n - 1
-    column_1.insert(idx, @disk)
-    display
+    @column_from[@column_from.index(@disk)] = nil
+    @column_to.shift
+    idx = @column_to.find_index { |d| !d.nil? } || @n - 1
+    @column_to.insert(idx, @disk)
   end
 
   def winning?
-    @board[2] == (1..@n).map {|n| "o" * n }
+    @board[2] == (1..@n).map { |n| 'o' * n }
   end
 end
 
