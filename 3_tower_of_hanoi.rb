b0718b101735aaee0740cbe7bc20ece047a2f601
agathasta <kalidonia@hotmail.com>
2020-11-11 10:24:30 +0100

    Add final instructions to ToH v.2.1

--- 3_tower_of_hanoi.rb
+++ 3_tower_of_hanoi.rb
@@ -2,7 +2,17 @@
 
 class Game
   def initialize
+    intro
     @board = Board.new
+  end
+
+  def intro
+    puts
+    puts '***** TOWER OF HANOI *****'
+    puts 'The objective of the puzzle is to move all disks to another rod,'
+    puts 'obeying the following simple rules:'
+    puts '1. Only one disk can be moved at a time.'
+    puts '2. No larger disk may be placed on top of a smaller disk.'
   end
 
   def play
@@ -20,12 +30,24 @@ class Board
   attr_accessor :board
 
   def initialize
-    puts 'How many disks?'
-    @n = gets.chomp.to_i
+    loop do
+      puts 'How many disks do you want to play with? (minimum 3)'
+      print '> '
+      @n = gets.chomp.to_i
+      break if @n > 2
+    end
     @board = Array.new(3) { Array.new(@n) }
     @board[0] = (1..@n).map { |n| 'o' * n }
-    @round = 1
     display
+    intro_moves
+  end
+
+  def intro_moves
+    puts 'To move the disks, input two numbers separated by a space:'
+    puts    'the first one is the rod you take the disk FROM,'
+    puts    'the second is the rod you move the disk TO.'
+    puts 'V.gr.: 1 3 will move the disk from rod 1 to rod 3.'
+    puts
   end
 
   def display
@@ -41,31 +63,27 @@ class Board
   end
 
   def coordinates
-    if @round == 1
-      puts 'To move the disks, input two numbers separated by a space:'
-      puts    'the first one is the column you take the disk FROM,'
-      puts    'the second is the column you move the disk TO.'
-      puts 'V.gr.: 1 3 will move the disk from column 1 to column 3.'
-      puts
-      @round = 'x'
-    end
     loop do
-      puts 'Where to move?'
+      puts 'Move disk:'
+      print '> '
       coord = gets.chomp.split(' ').map { |c| c.to_i - 1 }
-      @column_nr_from = coord[0]
-      @column_nr_to = coord[1]
+      @rod_nr_from = coord[0]
+      @rod_nr_to = coord[1]
       next unless check_coordinates_format
 
-      @column_from = @board[@column_nr_from]
-      @column_to = @board[@column_nr_to]
-      @disk = @column_from.find { |d| !d.nil? }
+      @rod_from = @board[@rod_nr_from]
+      @rod_to = @board[@rod_nr_to]
+      @disk = @rod_from.find { |d| !d.nil? }
       break if check_coordinates_position
     end
   end
 
   def check_coordinates_format
-    if !@column_nr_from.nil? && !@column_nr_to.nil?
-      true if (0..2).include?(@column_nr_from) && (0..2).include?(@column_nr_to)
+    if !@rod_nr_from.nil? && !@rod_nr_to.nil?
+      if (0..2).include?(@rod_nr_from) && (0..2).include?(@rod_nr_to)
+        true
+      else puts 'There are only 3 rods ;)'
+      end
     else
       puts 'Wrong input!'
     end
@@ -76,26 +94,26 @@ class Board
   end
 
   def check_coordinates_start
-    if @column_from[@n - 1].nil?
-      puts "No disk on column #{@column_nr_from + 1}"
+    if @rod_from[@n - 1].nil?
+      puts "No disk on rod #{@rod_nr_from + 1}!"
     else
       true
     end
   end
 
   def check_coordinates_finish
-    if @column_to[@n - 1].nil? || @disk < @column_to.find { |d| !d.nil? }
+    if @rod_to[@n - 1].nil? || @disk < @rod_to.find { |d| !d.nil? }
       true
     else
-      puts 'A disk can only be put on top of a bigger one'
+      puts 'A disk can only be put on top of a bigger one! (or on an empty rod)'
     end
   end
 
   def new_board
-    @column_from[@column_from.index(@disk)] = nil
-    @column_to.shift
-    idx = @column_to.find_index { |d| !d.nil? } || @n - 1
-    @column_to.insert(idx, @disk)
+    @rod_from[@rod_from.index(@disk)] = nil
+    @rod_to.shift
+    idx = @rod_to.find_index { |d| !d.nil? } || @n - 1
+    @rod_to.insert(idx, @disk)
   end
 
   def winning?
