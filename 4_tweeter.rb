a41c8c235e91e0a5781e5d0ec5dee987f2d6c38e
agathasta <kalidonia@hotmail.com>
2020-11-17 23:18:56 +0100

    Correct style

--- 4_tweeter.rb
+++ 4_tweeter.rb
@@ -1,30 +1,31 @@
+# frozen_string_literal: true
+
 class Tweeter
   include Enumerable
 
-    def initialize
-        @tweets = []
-    end
+  def initialize
+    @tweets = []
+  end
 
-    def tweet(message)
-        @tweets << message.chars[0..143].join
-    end
+  def tweet(message)
+    @tweets << message.chars[0..143].join
+  end
 
-    def each(&block)
-      @tweets.each(&block)
-      @tweets
-    end
+  def each(&block)
+    @tweets.each(&block)
+    @tweets
+  end
 end
 
 t = Tweeter.new
 
 # Add some tweets to our @tweets array
-t.tweet("first message")
-t.tweet("second message")
+t.tweet('first message')
+t.tweet('second message')
 
 # Use Enumerable methods on the Tweeter instance
-t.each{|msg| puts msg}
+t.each { |msg| puts msg }
 
 # Note: You should NOT have defined this method explicitly!
 #   ...it should come from Enumerable by default.
-p t.map{|msg| msg.upcase}
-
+p t.map(&:upcase)
