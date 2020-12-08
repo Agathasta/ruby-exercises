9880e27a2c52a95c26f2b1dc402210b25ead3f26
agathasta <kalidonia@hotmail.com>
2020-12-08 17:37:20 +0100

    Working solution without hints

--- 5_merge_sort.rb
+++ 5_merge_sort.rb
@@ -1,34 +1,28 @@
 require 'pry'
 
-def merge_sort(arr)
-
+def merge_sort(arr, sorted_arr = [])
   if arr.size < 2
     arr
   else
-    left = merge_sort(arr[0...arr.size/2])
-    right = merge_sort(arr[arr.size/2..-1])
-    merge(left, right)
-  end
-end
-
-def merge(left, right, sorted_arr = [])
-  binding.pry
-  2.times do
-    if right.empty?
-      sorted_arr << left.shift
-    elsif left.empty?
-      sorted_arr << right.shift
-    else
-      compare = left <=> right
-      if compare == -1
-        sorted_arr << left.shift
-      elsif compare == 1
+    left = merge_sort(arr[0...arr.size / 2])
+    right = merge_sort(arr[arr.size / 2..-1])
+    (left.size + right.size).times do
+      if left.empty?
         sorted_arr << right.shift
+      elsif right.empty?
+        sorted_arr << left.shift
+      else
+        compare = left <=> right
+        if compare == -1
+          sorted_arr << left.shift
+        else
+          sorted_arr << right.shift
+        end
       end
     end
+    sorted_arr
   end
-  sorted_arr
 end
 
-arr = [4, 2, 1, 5, 3]
-merge_sort(arr)
+array = [3, 65, 1, 98, 28, 4, 6, 22, 13, 1, 0]
+puts merge_sort(array)
