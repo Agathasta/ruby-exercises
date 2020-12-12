f929d80d71a472c02282f0c37994a28400979f26
agathasta <kalidonia@hotmail.com>
2020-12-12 11:26:24 +0100

    Polish style

--- 1_bubble_sort.rb
+++ 1_bubble_sort.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
 def bubble_sort(arr)
-    length = arr.length - 1
-    loop do
-        swap = false
-        for i in 0...length
-            if arr[i] > arr[i + 1]
-                x = arr[i + 1]
-                arr[i + 1] = arr[i]
-                arr[i] = x
-                swap = true
-            end
-        end
-        length -= 1
-        break if swap == false
+  length = arr.length - 1
+  loop do
+    swap = false
+    (0...length).each do |i|
+      next unless arr[i] > arr[i + 1]
+
+      x = arr[i + 1]
+      arr[i + 1] = arr[i]
+      arr[i] = x
+      swap = true
     end
-    p arr
+    length -= 1
+    break if swap == false
+  end
+  p arr
 end
 
-bubble_sort([4,3,78,2,0,2])
+bubble_sort([4, 3, 78, 2, 0, 2])
