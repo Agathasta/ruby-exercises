2b187cdb497f04df3945d2d3bed0270ba444a060
agathasta <kalidonia@hotmail.com>
2020-12-12 11:29:22 +0100

    Polish style

--- 4_deep_dupe.rb
+++ 4_deep_dupe.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Array
   def deep_dup
     map { |a| a.instance_of?(Array) ? a.deep_dup : a.dup }
@@ -6,19 +8,17 @@ end
 
 # TESTS
 
-=begin
-
-arr = [ 
-  [ 1, [ 2, 3 ] ], 
-  [ 4, 5, 6], 
-  [ [ 7, 8, [ 9, 10 ]], [11, 13] ]
-]
-duper = arr.deep_dup
-
-puts "object_id -> arr = #{arr.object_id} | duper = #{duper.object_id}"
-puts "arr[1] = #{arr[1]} | duper[1] = #{duper[1]}"
-puts "object_id -> arr[1] = #{arr[1].object_id} | duper[1] = #{duper[1].object_id}"
-puts "arr[2][0][2] = #{arr[2][0][2]} | duper[2][0][2] = #{duper[2][0][2]}"
-puts "object_id -> arr[2][0][2] = #{arr[2][0][2].object_id} | duper[2][0][2] = #{duper[2][0][2].object_id}"
-
-=end
+#
+# arr = [
+#   [ 1, [ 2, 3 ] ],
+#   [ 4, 5, 6],
+#   [ [ 7, 8, [ 9, 10 ]], [11, 13] ]
+# ]
+# duper = arr.deep_dup
+#
+# puts "object_id -> arr = #{arr.object_id} | duper = #{duper.object_id}"
+# puts "arr[1] = #{arr[1]} | duper[1] = #{duper[1]}"
+# puts "object_id -> arr[1] = #{arr[1].object_id} | duper[1] = #{duper[1].object_id}"
+# puts "arr[2][0][2] = #{arr[2][0][2]} | duper[2][0][2] = #{duper[2][0][2]}"
+# puts "object_id -> arr[2][0][2] = #{arr[2][0][2].object_id} | duper[2][0][2] = #{duper[2][0][2].object_id}"
+#
