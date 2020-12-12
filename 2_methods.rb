83b91cb633a182482ef6d48d2fef8db0cade1946
agathasta <kalidonia@hotmail.com>
2020-12-12 11:28:23 +0100

    Polish style

--- 2_methods.rb
+++ 2_methods.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 def my_reverse(str)
-  new_str = ""
-  for i in 0..str.length - 1
-    new_str << str[-1 -i]
+  new_str = ''
+  (0..str.length - 1).each do |i|
+    new_str << str[-1 - i]
   end
   new_str
 end
@@ -10,18 +12,17 @@ end
 
 def fibo(num)
   fibs = [0, 1]
-  for i in 2..num - 1
+  (2..num - 1).each do |i|
     fibs << (fibs[i - 1]) + (fibs[i - 2])
   end
   p fibs
-
 end
 
 # fibo(8)
 
-def my_benchmark(times)
+def my_benchmark(times, &block)
   time_beginning = Time.now
-  times.times { yield }
+  times.times(&block)
   time_end = Time.now
   puts "That took #{time_end - time_beginning} seconds"
 end
