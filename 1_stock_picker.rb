bb343c6c687605c961bdafa25cdd60b573271596
agathasta <kalidonia@hotmail.com>
2020-12-12 11:27:27 +0100

    Polish style

--- 1_stock_picker.rb
+++ 1_stock_picker.rb
@@ -1,15 +1,17 @@
-def stock_picker (stocks)
-    max_profit = 0
+# frozen_string_literal: true
 
-    for i in (0...stocks.length)
-        for j in ((i + 1)...stocks.length)
-            if stocks[j] - stocks[i] > max_profit
-                max_profit = stocks[j] - stocks[i]
-                days = [i,j]
-            end
-        end
+def stock_picker(stocks)
+  max_profit = 0
+
+  (0...stocks.length).each do |i|
+    ((i + 1)...stocks.length).each do |j|
+      if stocks[j] - stocks[i] > max_profit
+        max_profit = stocks[j] - stocks[i]
+        days = [i, j]
+      end
     end
-    p days
+  end
+  p days
 end
 
-stock_picker([17,3,6,9,15,8,6,2,10,1])
+stock_picker([17, 3, 6, 9, 15, 8, 6, 2, 10, 1])
