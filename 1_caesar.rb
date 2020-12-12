370b6191a6be477aa86364489af7e6e1d0aa3f6f
agathasta <kalidonia@hotmail.com>
2020-12-12 11:27:02 +0100

    Polish style

--- 1_caesar.rb
+++ 1_caesar.rb
@@ -1,27 +1,26 @@
+# frozen_string_literal: true
+
 def caesar_cipher(str, shift)
+  alphabet = ('a'..'z').to_a
+  code = []
 
-    alphabet = ("a".."z").to_a
-    code = []
-
-    str.split("").each do |letter|
-
-        if alphabet.include?(letter.downcase)
-            if letter == letter.upcase
-                index = alphabet.find_index(letter.downcase)
-                new_index = (index + shift + 26) % alphabet.length
-                code << alphabet[new_index].upcase
-            elsif letter == letter.downcase
-                index = alphabet.find_index(letter)
-                new_index = (index + shift + 26) % alphabet.length
-                code << alphabet[new_index]
-            end
-        else
-            code << letter
-        end
-
+  str.split('').each do |letter|
+    if alphabet.include?(letter.downcase)
+      if letter == letter.upcase
+        index = alphabet.find_index(letter.downcase)
+        new_index = (index + shift + 26) % alphabet.length
+        code << alphabet[new_index].upcase
+      elsif letter == letter.downcase
+        index = alphabet.find_index(letter)
+        new_index = (index + shift + 26) % alphabet.length
+        code << alphabet[new_index]
+      end
+    else
+      code << letter
     end
-    return code.join("")
+  end
+  code.join('')
 end
 
-caesar_cipher("What a string!", 5)
-caesar_cipher("What a string!", -5)
+caesar_cipher('What a string!', 5)
+caesar_cipher('What a string!', -5)
