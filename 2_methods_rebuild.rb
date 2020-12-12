be566079133682d4322fa0fd015e6e3c9e230439
agathasta <kalidonia@hotmail.com>
2020-12-12 11:34:54 +0100

    Polish style

--- 2_methods_rebuild.rb
+++ 2_methods_rebuild.rb
@@ -14,10 +14,10 @@ def my_each(proc = nil)
   self
 end
 
-# [1,2,5].my_each{ |item| puts item**2 }
+[1, 2, 5].my_each { |item| puts item**2 }
 
-# my_proc = Proc.new{|item| puts item**2}
-# [1,2,5].my_each(my_proc)
+my_proc = proc { |item| puts item**2 }
+[1, 2, 5].my_each(my_proc)
 
 def my_map(proc = nil)
   arr = []
@@ -31,10 +31,10 @@ def my_map(proc = nil)
   arr
 end
 
-# [1,2,5].my_map {|item| item**2}
+[1, 2, 5].my_map { |item| item**2 }
 
-# my_proc = Proc.new{|item| item**2}
-# [1,2,5].my_map(my_proc)
+my_proc = proc { |item| item**2 }
+[1, 2, 5].my_map(my_proc)
 
 def my_select(proc = nil)
   arr = []
@@ -48,10 +48,10 @@ def my_select(proc = nil)
   arr
 end
 
-# [1,2,5].my_select{|item| item.even?}
+[1, 2, 5].my_select(&:even?)
 
-# my_proc = Proc.new{|item| item.even?}
-# [1,2,5].my_select(my_proc)
+my_proc = proc { |item| item.even? }
+[1, 2, 5].my_select(my_proc)
 
 def my_all?(proc = nil)
   my_each do |e|
@@ -64,10 +64,10 @@ def my_all?(proc = nil)
   true
 end
 
-# [4,2,6].my_all?{|item| item.even?}
+[4, 2, 6].my_all?(&:even?)
 
-# my_proc = Proc.new{|item| item.even?}
-# [1,2,5].my_all?(my_proc)
+my_proc = proc { |item| item.even? }
+[1, 2, 5].my_all?(my_proc)
 
 def my_inject(acc = 0, proc = nil)
   acc = self[0] unless acc.zero?
@@ -81,7 +81,7 @@ def my_inject(acc = 0, proc = nil)
   acc
 end
 
-# [1,2,5].my_inject(0) {|memo, item| memo + item}
+[1, 2, 5].my_inject(0) { |memo, item| memo + item }
 
-# my_proc = Proc.new{|memo, item| memo + item}
-# [1,2,5].my_inject(0, my_proc)
+my_proc = proc { |memo, item| memo + item }
+[1, 2, 5].my_inject(0, my_proc)
